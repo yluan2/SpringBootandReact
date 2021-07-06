bb3e9fec209a9277ac0e14320df56de994e0c452
yluan2 <luanyx1126@gmail.com>
2021-07-06 17:28:21 -0400

    Coonected to H2 in-memory databse and DONE

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -4,31 +4,31 @@ class TodoDataService{
 
     retrieveAllTodos(username){
         //return promise back
-        return axios.get(`http://localhost:8080/users/${username}/todos`); 
+        return axios.get(`http://localhost:8080/jpa/users/${username}/todos`); 
                
     }
 
     retrieveTodo(username, id){
         //return promise back
-        return axios.get(`http://localhost:8080/users/${username}/todos/${id}`); 
+        return axios.get(`http://localhost:8080/jpa/users/${username}/todos/${id}`); 
                
     }
 
     deleteTodo(username, id){
         //return promise back
-        return axios.delete(`http://localhost:8080/users/${username}/todos/${id}`); 
+        return axios.delete(`http://localhost:8080/jpa/users/${username}/todos/${id}`); 
                
     }
 
     updateTodo(username, id, todo){
         //return promise back
-        return axios.put(`http://localhost:8080/users/${username}/todos/${id}`, todo); 
+        return axios.put(`http://localhost:8080/jpa/users/${username}/todos/${id}`, todo); 
                
     }
 
     createTodo(username, todo){
         //return promise back
-        return axios.post(`http://localhost:8080/users/${username}/todos/`, todo)
+        return axios.post(`http://localhost:8080/jpa/users/${username}/todos/`, todo)
                
     }
 
